e95880d99955f3df68dafa27ea64e3c8a357f376
opilane <opilane@HP-CZC6297BBB>
2022-02-03 12:12:36 +0200

    b

--- Tunniplaan.py
+++ Tunniplaan.py
@@ -69,29 +69,29 @@ p27=Button(text="Rühma juhatuja tund", font="Calibri 26",bg="skyblue",command=l
 #Teisipäev
 p6=Button(text="Inglise keel Grupp1", font="Calibri 26",bg="floralwhite",command=lambda:kirjeldus_aknasse("pip.png","Inglise keel Grupp 1")).grid(row=3,column=2,columnspan=2,sticky=N+S+W+E)
 p7=Button(text="Inglise keel Grupp2", font="Calibri 26",bg="mediumorchid",command=lambda:kirjeldus_aknasse("pip.png","Inglise keel Grupp 2")).grid(row=4,column=2,columnspan=2,sticky=N+S+W+E)
-p8=Button(text="Operatsioonisüsteemide alused", font="Calibri 26",bg="darkorchid",command=lambda:kirjeldus_aknasse("bun.png","Operatsioonisusteemide alused")).grid(row=3,column=4,columnspan=2,rowspan=2,sticky=N+S+W+E)
-p10=Button(text="Kehaline kasvatus", font="Calibri 26",bg="palevioletred",command=lambda:kirjeldus_aknasse("Kehaline kasvatus")).grid(row=3,column=7,columnspan=2,rowspan=2,sticky=N+S+W+E)
-p11=Button(text="Eesti keel Grupp1", font="Calibri 26",bg="mediumpurple",command=lambda:kirjeldus_aknasse("Eesti keel Grupp 1")).grid(row=3,column=9,sticky=N+S+W+E)
-p12=Button(text="Eesti keel Grupp2", font="Calibri 26",bg="darkgrey",command=lambda:kirjeldus_aknasse("Eesti keel Grupp 2")).grid(row=4,column=9,sticky=N+S+W+E)
-p13=Button(text="Ajalugu", font="Calibri 26",bg="khaki",command=lambda:kirjeldus_aknasse("Ajalugu, inimgeograafia")).grid(row=3,column=10,rowspan=2,sticky=N+S+W+E)
+p8=Button(text="Operatsioonisüsteemide alused", font="Calibri 26",bg="darkorchid",command=lambda:kirjeldus_aknasse("kkk.png","Operatsioonisusteemide alused")).grid(row=3,column=4,columnspan=2,rowspan=2,sticky=N+S+W+E)
+p10=Button(text="Kehaline kasvatus", font="Calibri 26",bg="palevioletred",command=lambda:kirjeldus_aknasse("kuki.png","Kehaline kasvatus")).grid(row=3,column=7,columnspan=2,rowspan=2,sticky=N+S+W+E)
+p11=Button(text="Eesti keel Grupp1", font="Calibri 26",bg="mediumpurple",command=lambda:kirjeldus_aknasse("big.png","Eesti keel Grupp 1")).grid(row=3,column=9,sticky=N+S+W+E)
+p12=Button(text="Eesti keel Grupp2", font="Calibri 26",bg="darkgrey",command=lambda:kirjeldus_aknasse("big.png","Eesti keel Grupp 2")).grid(row=4,column=9,sticky=N+S+W+E)
+p13=Button(text="Ajalugu", font="Calibri 26",bg="khaki",command=lambda:kirjeldus_aknasse("mimi.png","Ajalugu, inimgeograafia")).grid(row=3,column=10,rowspan=2,sticky=N+S+W+E)
 
 #Kolmapäev
 p14=Button(text="Programmeerimise alused Grupp1", font="Calibri 26",bg="skyblue",command=lambda:kirjeldus_aknasse("image-7.png","Programmeerimise alused ")).grid(row=5,column=2,columnspan=3,sticky=N+S+W+E)
 p15=Button(text="Multimeedia Grupp2", font="Calibri 26",bg="cornflowerblue",command=lambda:kirjeldus_aknasse("mu.png","Multimeedia")).grid(row=6,column=2,columnspan=3,sticky=N+S+W+E)
 p16=Button(text="Multimeedia Grupp1", font="Calibri 26",bg="cornflowerblue",command=lambda:kirjeldus_aknasse("mu.png","Multimeedia")).grid(row=6,column=6,columnspan=3,sticky=N+S+W+E)
 p17=Button(text="Programmeerimise alused Grupp2", font="Calibri 26",bg="skyblue",command=lambda:kirjeldus_aknasse("image-7.png","Programmeerimise alused ")).grid(row=5,column=6,columnspan=3,sticky=N+S+W+E)
-p18=Button(text="Kunstiained", font="Calibri 26",bg="orchid",command=lambda:kirjeldus_aknasse("Kunstiained")).grid(row=5,column=9,columnspan=2,rowspan=2,sticky=N+S+W+E)
+p18=Button(text="Kunstiained", font="Calibri 26",bg="orchid",command=lambda:kirjeldus_aknasse("bu.png","Kunstiained")).grid(row=5,column=9,columnspan=2,rowspan=2,sticky=N+S+W+E)
 
 #Neljapäev
-p19=Button(text="Andmebaasisüsteemide alused", font="Calibri 26",bg="salmon",command=lambda:kirjeldus_aknasse("Andmebaasisusteemide alused")).grid(row=7,column=2,columnspan=5,rowspan=2,sticky=N+S+W+E)
-p20=Button(text="Ajalugu", font="Calibri 26",bg="khaki",command=lambda:kirjeldus_aknasse("Ajalugu, inimgeograafia")).grid(row=7,column=6,rowspan=2,sticky=N+S+W+E)
-p21=Button(text="Eesti keel Grupp1", font="Calibri 26",bg="mediumpurple",command=lambda:kirjeldus_aknasse("Eesti keel Grupp 1")).grid(row=7,column=7,sticky=N+S+W+E)
-p22=Button(text="Eesti keel Grupp2", font="Calibri 26",bg="darkgrey",command=lambda:kirjeldus_aknasse("Eesti keel Grupp 2")).grid(row=8,column=7,sticky=N+S+W+E)
+p19=Button(text="Andmebaasisüsteemide alused", font="Calibri 26",bg="salmon",command=lambda:kirjeldus_aknasse("puk.png","Andmebaasisusteemide alused")).grid(row=7,column=2,columnspan=5,rowspan=2,sticky=N+S+W+E)
+p20=Button(text="Ajalugu", font="Calibri 26",bg="khaki",command=lambda:kirjeldus_aknasse("mimi.png","Ajalugu, inimgeograafia")).grid(row=7,column=6,rowspan=2,sticky=N+S+W+E)
+p21=Button(text="Eesti keel Grupp1", font="Calibri 26",bg="mediumpurple",command=lambda:kirjeldus_aknasse("big.png","Eesti keel Grupp 1")).grid(row=7,column=7,sticky=N+S+W+E)
+p22=Button(text="Eesti keel Grupp2", font="Calibri 26",bg="darkgrey",command=lambda:kirjeldus_aknasse("big.png","Eesti keel Grupp 2")).grid(row=8,column=7,sticky=N+S+W+E)
 
 #Reede
-p23=Button(text="Vene keel", font="Calibri 26",bg="greenyellow",command=lambda:kirjeldus_aknasse("Keel ja kirjandus")).grid(row=9,column=3,columnspan=2,rowspan=2,sticky=N+S+W+E)
-p24=Button(text="Matemaatika", font="Calibri 26",bg="lightpink",command=lambda:kirjeldus_aknasse("Matemaatika")).grid(row=9,column=6,columnspan=2,rowspan=2,sticky=N+S+W+E)
-p25=Button(text="Suhtlemine ja kliienditeenindus", font="Calibri 26",bg="mediumslateblue",command=lambda:kirjeldus_aknasse("Suhtlemine ja klienditeenindus")).grid(row=9,column=8,columnspan=2,rowspan=2,sticky=N+S+W+E)
-p26=Button(text="Ajalugu", font="Calibri 26",bg="khaki",command=lambda:kirjeldus_aknasse("Ajalugu, inimgeograafia")).grid(row=9,column=10,rowspan=2,sticky=N+S+W+E)
+p23=Button(text="Vene keel", font="Calibri 26",bg="greenyellow",command=lambda:kirjeldus_aknasse("eel.png","Keel ja kirjandus")).grid(row=9,column=3,columnspan=2,rowspan=2,sticky=N+S+W+E)
+p24=Button(text="Matemaatika", font="Calibri 26",bg="lightpink",command=lambda:kirjeldus_aknasse("mmm.png","Matemaatika")).grid(row=9,column=6,columnspan=2,rowspan=2,sticky=N+S+W+E)
+p25=Button(text="Suhtlemine ja kliienditeenindus", font="Calibri 26",bg="mediumslateblue",command=lambda:kirjeldus_aknasse("bubu.png","Suhtlemine ja klienditeenindus")).grid(row=9,column=8,columnspan=2,rowspan=2,sticky=N+S+W+E)
+p26=Button(text="Ajalugu", font="Calibri 26",bg="khaki",command=lambda:kirjeldus_aknasse("mimi.png","Ajalugu, inimgeograafia")).grid(row=9,column=10,rowspan=2,sticky=N+S+W+E)
 
 aken.mainloop()
